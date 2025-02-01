a4c783ec3b5ddc52e1763e711ed3d28606931039
marquisdegramont <142470232+marquisdegramont@users.noreply.github.com>
2025-02-01 15:06:48 +0530

    sample day 1 commits

---
+++ src/java8/Stream_Example.java
@@ -0,0 +1,8 @@
+package java8;
+
+public class Stream_Example {
+	public static void main(String[] args) {
+		System.out.println("hello world");
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author 91891
+ *
+ */
+module java8_Interview_questions {
+}
